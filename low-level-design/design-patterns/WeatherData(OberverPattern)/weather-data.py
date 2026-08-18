4eda20ae563bd95e4e212e67c9b3217a6f009f53
Shreya Menneni <menneni.shreya@gmail.com>
2026-08-18 23:36:19 +0530

    enhancements in weather-data.py

--- low-level-design/design-patterns/WeatherData(OberverPattern)/weather-data.py
+++ low-level-design/design-patterns/WeatherData(OberverPattern)/weather-data.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 class Observer(ABC):
     @abstractmethod
-    def update(self, temperature, humidity, pressure) -> None:
+    def update(self) -> None:
         pass
 
-class Subject(ABC):
+class Subject():
     def __init__(self) -> None:
         self.observers = []
 
@@ -18,9 +18,9 @@ class Subject(ABC):
         else:
             raise ValueError(f"Observer {observer} not found")
 
-    @abstractmethod
     def notify_observer(self) -> None:
-        pass
+        for observer in self.observers:
+            observer.update()
 
 
 class WeatherData(Subject):
@@ -39,12 +39,11 @@ class WeatherData(Subject):
     def get_pressure(self) -> float:
         return self.pressure
 
-    def measurements_changed(self) -> None:
+    def measurements_changed(self, temperature: float, humidity: float, pressure: float) -> None:
+        self.temperature = temperature
+        self.humidity = humidity
+        self.pressure = pressure
         self.notify_observer()
-
-    def notify_observer(self) -> None:
-        for observer in self.observers:
-            observer.update(self.get_temperature(), self.get_humidity(), self.get_pressure())
 
 
 class Display(ABC):
@@ -55,19 +54,24 @@ class Display(ABC):
 
 
 class CurrentConditionsDisplay(Observer, Display):
-    def update(self, temperature, humidity, pressure) -> None:
-        self.temperature = temperature
-        self.humidity = humidity
-        self.pressure = pressure
+    def __init__(self, weather_data: WeatherData) -> None:
+        self.weather_data = weather_data
+
+    def update(self) -> None:
+        self.temperature = self.weather_data.get_temperature()
+        self.humidity = self.weather_data.get_humidity()
+        self.pressure = self.weather_data.get_pressure()
         self.display()
 
     def display(self) -> None:
         print(f"Temperature: {self.temperature}, Humidity: {self.humidity}, Pressure: {self.pressure}")
 
-
 class StatisticsDisplay(Observer, Display):
-    def update(self, temperature, humidity, pressure) -> None:
-        self.temperature = temperature
+    def __init__(self, weather_data: WeatherData) -> None:
+        self.weather_data = weather_data
+
+    def update(self) -> None:
+        self.temperature = self.weather_data.get_temperature()
         self.display()
 
     def display(self) -> None:
@@ -75,9 +79,12 @@ class StatisticsDisplay(Observer, Display):
 
 
 class ForecastDisplay(Observer, Display):
-    def update(self, temperature, humidity, pressure) -> None:
-        self.humidity = humidity
-        self.pressure = pressure
+    def __init__(self, weather_data: WeatherData) -> None:
+        self.weather_data = weather_data
+
+    def update(self) -> None:
+        self.humidity = self.weather_data.get_humidity()
+        self.pressure = self.weather_data.get_pressure()
         self.display()
 
     def display(self) -> None:
@@ -86,12 +93,13 @@ class ForecastDisplay(Observer, Display):
 
 if __name__ == "__main__":
     weather_data = WeatherData(20, 60, 1013)
-    current_conditions_display = CurrentConditionsDisplay()
-    statistics_display = StatisticsDisplay()
-    forecast_display = ForecastDisplay()
+    current_conditions_display = CurrentConditionsDisplay(weather_data)
+    statistics_display = StatisticsDisplay(weather_data)
+    forecast_display = ForecastDisplay(weather_data)
 
     weather_data.register_observer(current_conditions_display)
     weather_data.register_observer(statistics_display)
     weather_data.register_observer(forecast_display)
 
-    weather_data.measurements_changed()
+    weather_data.notify_observer()
+    weather_data.measurements_changed(21, 65, 1014)
